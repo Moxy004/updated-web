9f51b020db645bb267cc0e327e3c9642e2c5f164
Moxy004 <159922815+Moxy004@users.noreply.github.com>
2024-12-02 00:01:44 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", './newpage.html', './index.html', './function.html', './exponential.html', './logarithmic.html', './interest.html', './stocks.html', './normal.html', './normaldistribution.html', './poisson.html', './scalar.html', './recti.html', './2d3d.html', './newton.html', 'stoic.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
